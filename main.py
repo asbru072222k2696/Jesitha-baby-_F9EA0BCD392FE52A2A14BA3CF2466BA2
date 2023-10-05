2762dac0cb51be58faa8b9c7986a9848ea7c8e6c
asbru072222k2696 <145265362+asbru072222k2696@users.noreply.github.com>
2023-10-05 12:46:10 +0530

    Unit 1 challenges

---
+++ main.py
@@ -0,0 +1,5 @@
+#program to find factorial of a given number
+def factorial (n):
+  return 1 if(n==1) or (n==0) else n* factorial (n-1)
+  num=7
+  print ("The factorial of",num,"is", factorial (num))
